410695a3ef8b571b47abc7547fc80f4e1a6e75d7
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-23 20:56:25 -0500

    Add logic to count palindromic substrings
    Implement logic to count palindromic substrings.

--- countSubstrings.py
+++ countSubstrings.py
@@ -15,3 +15,11 @@ class Solution:
         #     r += 1
         #
         # return res
+        
+        res = 0
+        for i in range(len(s)):
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
