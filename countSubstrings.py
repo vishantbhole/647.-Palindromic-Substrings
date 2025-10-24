f15cd0eddaa85d8d1c11147aadf4f17d500d9d3b
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-23 20:55:55 -0500

    Update countSubstrings.py

--- countSubstrings.py
+++ countSubstrings.py
@@ -2,3 +2,16 @@
 # 647. Palindromic Substrings
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        # l = 0
+        # r = 1
+        # res = 0
+        # for i in range(l, len(s)):
+        #     for j in range(r, len(s)+1):
+        #         string = s[i:j]
+        #         if(string == string[::-1]):
+        #             res += 1
+        #             l += 1
+        #
+        #     r += 1
+        #
+        # return res
