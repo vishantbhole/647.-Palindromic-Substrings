257de8673208a642ac909e47ab443092a717cf91
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-10-23 20:57:23 -0500

    Implement main block for testing
    Add a main block to test countSubstrings function

--- countSubstrings.py
+++ countSubstrings.py
@@ -32,3 +32,9 @@ class Solution:
                 r += 1
 
         return res
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "abc"
+
+    print("Output is : ", sol.countSubstrings(s))
